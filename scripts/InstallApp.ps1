f54e3c163279ac1d03e364f89f46ad3a4622476f
Carlos Baquero <karlosmario0123@gmail.com>
2021-05-11 21:20:58 -0500

    deploy prueba 6

--- scripts/InstallApp.ps1
+++ scripts/InstallApp.ps1
@@ -9,4 +9,4 @@ sl C:\bingo\projAspAngular
 
 # Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
 # To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
-C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\ExploringAspNetCore\publish}
+C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\bingo\publish}
